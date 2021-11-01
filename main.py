9bb5e1e593ef783b4d5ff198b15ed00c65c4d92d
HajLuk <74139298+HajLuk@users.noreply.github.com>
2021-11-01 21:52:50 +0100

    Less displayed days on the X-axis

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def expfig(x, xshift, a, b):  # exponential for set of values (used for visualiz
 download = True  # 'True' means that the current data from mzcr.cz will be downloaded
 N0 = 226-1  # we only wanna visualize from this date (8th of September)
 Nfit = N0 + 356  # previously N0+85, the "next wave" is N0+291 (June 26th 2021), next one is N0+335 (Aug 9th 2021), after adjustment N0+356 (Aug 30th 2021)
-days_step = 3  # should we show each day?
+days_step = 4  # should we show each day?
 
 # download the file (optional), open it and import its contents
 c19DataFName = 'covid19data.csv'
