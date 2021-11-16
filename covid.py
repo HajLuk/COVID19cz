0d8e872df9374eb74827d4950144d24acf9190af
HajLuk <74139298+HajLuk@users.noreply.github.com>
2021-11-16 01:34:02 +0100

    Overview of hospitalized patients added

--- covid.py
+++ covid.py
@@ -10,7 +10,6 @@ import matplotlib.ticker as ticker
 def download_data(data_path):
     """
     This function  download data from MZCR, form single dataframe, store it and return it.
-
     :param data_path:
     :return:
     """
@@ -27,7 +26,6 @@ def handle_fig(func):
     This is a wrapper for drawing functions.
     All drawing functions should use it as a decorator.
     It display and/or save figure.
-
     :param func:
     :return:
     """
@@ -47,7 +45,6 @@ def get_exponential(dataset, col_name, new_col_name, start=False, stop=False, ho
     This si a function, that add new series to the given dataset.
     The added series is a exponential "prediction" of the target series.
     Note: it also extends the dataset by prediction horizon.
-
     :param dataset: pandas dataframe
     :param col_name: name of the series to "predict"
     :param new_col_name: name for the new series - the "predicted" one
@@ -76,7 +73,6 @@ def basic_view(dataframe, **kwargs):
     """
     The basic overview. It creates subset from the given date.
     It draws some basic series and their "prediction" for short horizon.
-
     :param dataframe:
     :param kwargs:
     :return:
@@ -101,7 +97,7 @@ def basic_view(dataframe, **kwargs):
         new_column_name = "{}_exp".format(td["n"])
         augmented_subset = get_exponential(subset, td["n"], new_column_name, start=DATES["wave3"], horizon=14)
         ax.plot(augmented_subset.index, augmented_subset[td["n"]], td["c"], linestyle="-", marker="x", label=td["l"])
-        ax.plot(augmented_subset.index, augmented_subset[new_column_name], ':{}'.format(td["c"]),)
+        ax.plot(augmented_subset.index, augmented_subset[new_column_name], ':{}'.format(td["c"]))
     plt.xlim(augmented_subset.index[0], augmented_subset.index[-1])
     plt.ylim(0, subset["aktualne_nakazenych"].max()*1.05)
     ax.xaxis.set_major_locator(plt.MaxNLocator(100))
@@ -110,6 +106,52 @@ def basic_view(dataframe, **kwargs):
     plt.grid()
     plt.legend()
     ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: "{}k".format(int(int(x) / 1000))))
+    plt.tight_layout()
+    return fig
+
+@handle_fig
+def hospi_view(dataframe, **kwargs):
+    """
+    The basic overview. It creates subset from the given date.
+    It draws some basic series and their "prediction" for short horizon.
+    :param dataframe:
+    :param kwargs:
+    :return:
+    """
+    FIGSIZE = (19, 8)
+    WINDOWNAME = "Hospitalizace"
+    subset = dataframe[dataframe.index > DATES["new_age"]]
+
+    fig = plt.figure(WINDOWNAME, figsize=FIGSIZE)
+    ax = plt.gca()
+
+    to_draw = [
+        # "n: column name, "l": plot label, "c": plot color
+        {"n": "stav_tezky", "l": "Hospitalizováni ve vážném stavu", "c": "k"},
+        {"n": "stav_stredni", "l": "Hospitalizováni se středně těžkými příznaky", "c": "r"},
+        {"n": "stav_lehky", "l": "Hospitalizováni s lehkými příznaky", "c": "y"},
+        {"n": "stav_bez_priznaku", "l": "Hospitalizováni bez příznaků", "c": "g"},
+    ]
+
+    #sumtd = np.zeros(to_draw.length())
+    subset = dataframe[dataframe.index > DATES["new_age"]]
+    subset["sum td next"] = subset["pocet_hosp"] - subset["pocet_hosp"]
+    for td in to_draw:
+        subset["sum td prev"] = subset["sum td next"]
+        subset["sum td next"] += subset[td["n"]]
+        new_column_name = "{}_exp".format(td["n"])
+        augmented_subset = get_exponential(subset, "sum td next", new_column_name, start=DATES["wave3"], horizon=14)
+        ax.fill_between(subset.index, subset["sum td prev"], subset["sum td next"], color=td["c"], label=td["l"])
+        ax.plot(augmented_subset.index, augmented_subset[new_column_name], ':{}'.format(td["c"]))
+    plt.xlim(augmented_subset.index[0], augmented_subset.index[-1])
+    plt.ylim(0, subset["pocet_hosp"].max()*1.05)
+    ax.xaxis.set_major_locator(plt.MaxNLocator(100))
+    ax.yaxis.set_major_locator(plt.MaxNLocator(25))
+    plt.xticks(rotation=90)
+    plt.grid()
+    plt.legend()
+    ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    # ax.get_yaxis().set_major_formatter(ticker.FuncFormatter(lambda x, p: "{}k".format(int(int(x) / 1000))))
     plt.tight_layout()
     return fig
 
@@ -141,5 +183,6 @@ if __name__ == "__main__":
     # print(dataframe.keys())
 
     basic_view(dataframe, display=True, filename="basic_overview.png")
+    hospi_view(dataframe, display=True, filename="hospi_overview.png")
 
     plt.show()
